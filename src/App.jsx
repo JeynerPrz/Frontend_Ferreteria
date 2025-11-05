2a4eb8df4e977d1eff6acfd6f6c5c8fa6b074bda
JeynerPrz <jeynerperezalvarez@gmail.com>
2025-11-04 19:51:21 -0600

    confi

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Importar componente Encabezado
+import Encabezado from "./components/navegacion/Encabezado";
+// Importar las vistas
+import Login from "./views/Login";
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categoria";
+import Productos from "./views/Producto";
+import Catalogo from "./views/Catalogo";
+import Ventas from "./views/Venta";
+import Clientes from "./views/Cliente";
+import Empleados from "./views/Empleado";
+import Compras from "./views/Compra";
+import Usuarios from "./views/Usuario";
+// Importar archivo de estilos
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Encabezado />
+      <main className="margen-superior-main">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/clientes" element={<Clientes />} /> 
+          <Route path="/compras" element={<Compras />} /> 
+          <Route path="/empleados" element={<Empleados />} />
+          <Route path="/usuarios" element={<Usuarios />} /> 
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
