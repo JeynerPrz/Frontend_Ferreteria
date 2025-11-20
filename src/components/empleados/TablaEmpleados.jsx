ae6c734a6baae4e543fad0ff35730cb2f0135f1a
JeynerPrz <jeynerperezalvarez@gmail.com>
2025-11-19 21:56:35 -0600

    coreccion empleados

--- src/components/empleados/TablaEmpleados.jsx
+++ src/components/empleados/TablaEmpleados.jsx
@@ -1,32 +1,6 @@
-import React, { useState } from "react";
 import {Table, Spinner} from "react-bootstrap";
-import BotonOrden from "../ordenamiento/BotonOrden";
 
 const TablaEmpleados = ({empleados, cargando}) => {
-
-  const [orden, setOrden] = useState({ campo: "id_empleado", direccion: "asc" });
-
-  const manejarOrden = (campo) => {
-  setOrden((prev) => ({
-    campo,
-    direccion:
-      prev.campo === campo && prev.direccion === "asc" ? "desc" : "asc",
-  }));
-};
-
-const EmpleadosOrdenados = [...empleados].sort((a, b) => {
-  const valorA = a[orden.campo];
-  const valorB = b[orden.campo];
-
-  if (typeof valorA === "number" && typeof valorB === "number") {
-    return orden.direccion === "asc" ? valorA - valorB : valorB - valorA;
-  }
-
-  const comparacion = String(valorA).localeCompare(String(valorB));
-  return orden.direccion === "asc" ? comparacion : -comparacion;
-});
-
-
 
   if (cargando){
     return (
@@ -43,43 +17,16 @@ const EmpleadosOrdenados = [...empleados].sort((a, b) => {
     <Table striped bordered hover>
       <thead>
         <tr>
-   <BotonOrden campo="id_empleado" orden={orden} manejarOrden={manejarOrden}>
-  ID
-</BotonOrden>
-
-<BotonOrden campo="primer_nombre" orden={orden} manejarOrden={manejarOrden}>
- Primer Nombre
-</BotonOrden>
-
-<BotonOrden campo="segundo_nombre" orden={orden} manejarOrden={manejarOrden}>
-Segundo Nombre
-</BotonOrden>
-
-<BotonOrden campo= "primer_apellido" orden={orden} manejarOrden={manejarOrden}>
- Primer Apellido
-</BotonOrden>
-
-<BotonOrden campo="segundo_apellido" orden={orden} manejarOrden={manejarOrden}>
-Segundo Apellido
-</BotonOrden>
-
-<BotonOrden campo="celular" orden={orden} manejarOrden={manejarOrden}>
- Celular
-</BotonOrden>
-
-<BotonOrden campo="cargo" orden={orden} manejarOrden={manejarOrden}>
-Cargo
-</BotonOrden>
-
-<BotonOrden campo= "cedula" orden={orden} manejarOrden={manejarOrden}>
- Cedula
-</BotonOrden>
-
-<BotonOrden campo="fecha_contratacion" orden={orden} manejarOrden={manejarOrden}>
-Fecha Contratacion
-</BotonOrden>
-
-          <th>Acciónes</th>
+          <th>ID</th>
+          <th>Nombre 1</th>
+          <th>Nombre 2</th>
+          <th>Apellido 1</th>
+          <th>Apellido 2</th>
+          <th>Celular</th>
+          <th>Cargo</th>
+          <th>Cedula</th>
+          <th>Fecha Contratacion</th>
+          <th>Acción</th>
         </tr>
       </thead>
       <tbody>
@@ -93,7 +40,6 @@ Fecha Contratacion
              <td>{empleado.segundo_apellido}</td>
              <td>{empleado.celular}</td>
              <td>{empleado.cargo}</td>
-             <td>{empleado.cedula}</td>
              <td>{empleado.fecha_contratacion}</td>
             <td>Acción</td>
           </tr>
